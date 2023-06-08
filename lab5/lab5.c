2769f7a60a08f9482bc8ae3a77be544c8697f991
Felipe de Jesus <32593546+FJAConceicao@users.noreply.github.com>
2023-06-08 18:37:40 -0300

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -4,38 +4,89 @@
 
 #define NTHREADS 5
 
-/* Variaveis globais */
-int x = 0;
+/* Variaveis globais de controle e sincronização */
+int printed_message_counter = 0;
 pthread_mutex_t x_mutex;
-pthread_cond_t x_cond;
+pthread_cond_t x_cond_t1_t2;
+pthread_cond_t x_cond_t4_t5;
 
 /* Thread 1 */
-void *thread_um (void *t) {
+void *thread_um (void *t)
+{
     printf("Oi Maria!\n");
+
+    pthread_mutex_lock(&x_mutex);
+    printed_message_counter++;
+
+    if(printed_message_counter == 2)
+    {
+        pthread_cond_signal(&x_cond_t1_t2);
+    }
+    pthread_mutex_unlock(&x_mutex);
+
     pthread_exit(NULL);
 }
 
 /* Thread 2 */
-void *thread_dois (void *t) {
+void *thread_dois (void *t)
+{
     printf("Oi José!\n");
+
+    pthread_mutex_lock(&x_mutex);
+    printed_message_counter++;
+
+    if(printed_message_counter == 2)
+    {
+        pthread_cond_signal(&x_cond_t1_t2);
+    }
+    pthread_mutex_unlock(&x_mutex);
+
     pthread_exit(NULL);
 }
 
 /* Thread 3 */
 void *thread_tres (void *t) {
+
+    pthread_mutex_lock(&x_mutex);
+
+    if(printed_message_counter < 2) {
+        pthread_cond_wait(&x_cond_t1_t2, &x_mutex);
+    }
+
     printf("Sente-se por favor.\n");
+    pthread_cond_signal(&x_cond_t4_t5);
+
+    pthread_mutex_unlock(&x_mutex);
+
     pthread_exit(NULL);
 }
 
 /* Thread 4 */
 void *thread_quatro (void *t) {
+    pthread_mutex_lock(&x_mutex);
+
+    if(printed_message_counter < 3){
+        pthread_cond_wait(&x_cond_t4_t5, &x_mutex);
+    }
+  
     printf("Até mais José!\n");
+    pthread_mutex_unlock(&x_mutex);
+
     pthread_exit(NULL);
 }
 
 /* Thread 5 */
 void *thread_cinco (void *t) {
-    printf("Até mais Maria!\n", id);
+
+    pthread_mutex_lock(&x_mutex);
+
+    if(printed_message_counter < 3){
+        pthread_cond_wait(&x_cond_t4_t5, &x_mutex);
+    }
+  
+    printf("Até mais Maria!\n");
+    pthread_mutex_unlock(&x_mutex);
+    
     pthread_exit(NULL);
 }
 
@@ -45,9 +96,10 @@ int main(int argc, char *argv[]) {
   int i;
   pthread_t threads[NTHREADS];
 
-  /* Inicilaiza o mutex (lock de exclusao mutua) e a variavel de condicao */
+  /* Inicializa o mutex (lock de exclusao mutua) e a variavel de condicao */
   pthread_mutex_init(&x_mutex, NULL);
-  pthread_cond_init (&x_cond, NULL);
+  pthread_cond_init (&x_cond_t1_t2, NULL);
+  pthread_cond_init (&x_cond_t4_t5, NULL);
 
   /* Cria as threads */
   pthread_create(&threads[0], NULL, thread_um, NULL);
@@ -56,12 +108,14 @@ int main(int argc, char *argv[]) {
   pthread_create(&threads[3], NULL, thread_quatro, NULL);
   pthread_create(&threads[4], NULL, thread_cinco, NULL);
 
-  /* Espera todas as threads completarem */
-  for (i = 0; i < NTHREADS; i++) {
+  /* Espera todas as threads terminarem */
+  for (i = 0; i < NTHREADS; i++)
+  {
     pthread_join(threads[i], NULL);
   }
 
   /* Desaloca variaveis e termina */
   pthread_mutex_destroy(&x_mutex);
-  pthread_cond_destroy(&x_cond);
+  pthread_cond_destroy(&x_cond_t1_t2);
+  pthread_cond_destroy(&x_cond_t4_t5);
 }
