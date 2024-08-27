c3e6137dc9f2261f1ab5ef9f2e04a1b2050d0919
Felipe de Jesus <felipe.conceicao@g.globo>
2024-08-27 17:06:35 -0300

    lab2 fineshed

--- lab2/gera_dois_vets_rand.c
+++ lab2/gera_dois_vets_rand.c
@@ -3,15 +3,15 @@
 #include <time.h>
 
 #define MAX 1000 // valor máximo de um elemento do vetor
-#define TEXTO
+#define TEXTO // para imprimir vetores gerados gerados e produto interno calculado
 
 int main(int argc, char* argv[]) {
-    float *vetor1, *vetor2;
-    long int n;
-    double produtoInterno = 0.0;
-    int fator = 1;
-    FILE *descritorArquivo;
-    size_t ret;
+    float *vetor1, *vetor2; //vetores que serão gerados
+    long int n; //qtde de elementos dos vetores que serão gerados
+    double prodInt = 0.0; //guarda valor do produto interno dos vetores gerados
+    int fator = 1; //fator multiplicador para gerar números negativos
+    FILE *descritorArquivo; //descritor do arquivo de saida
+    size_t ret; //retorno da funcao de escrita no arquivo de saida
 
     // Recebe os argumentos de entrada
     if (argc < 3) {
@@ -21,10 +21,11 @@ int main(int argc, char* argv[]) {
     n = atoi(argv[1]);
 
     // Aloca memória para os vetores
-    vetor1 = (float*) malloc(sizeof(float) * n);
-    vetor2 = (float*) malloc(sizeof(float) * n);
+    int tamanhoVetores = sizeof(float)*n;
+    vetor1 = (float*) malloc(tamanhoVetores);
+    vetor2 = (float*) malloc(tamanhoVetores);
     if (!vetor1 || !vetor2) {
-        fprintf(stderr, "Erro de alocação de memória dos vetores\n");
+        fprintf(stderr, "--ERRO: Na alocação de memória dos vetores\n");
         return 2;
     }
 
@@ -33,53 +34,46 @@ int main(int argc, char* argv[]) {
     for (long int i = 0; i < n; i++) {
         vetor1[i] = (rand() % MAX) / 3.0 * fator;
         vetor2[i] = (rand() % MAX) / 3.0 * fator;
-        produtoInterno += vetor1[i] * vetor2[i];
+        prodInt += vetor1[i] * vetor2[i];
         fator *= -1;
     }
 
     // Escreve os vetores e o produto interno no arquivo binário
     descritorArquivo = fopen(argv[2], "wb");
     if (!descritorArquivo) {
-        fprintf(stderr, "Erro de abertura do arquivo\n");
+        fprintf(stderr, "--ERRO: Abertura do arquivo\n");
         return 3;
     }
 
     //escreva a dimensão no arquivo
     ret = fwrite(&n, sizeof(long int), 1, descritorArquivo);
-    if (ret < n) {
-        fprintf(stderr, "Erro de escrita da dimensão no arquivo\n");
-        return 4;
-    }
 
     ret = fwrite(vetor1, sizeof(float), n, descritorArquivo);
     if (ret < n) {
-        fprintf(stderr, "Erro de escrita do vetor 1 no arquivo\n");
+        fprintf(stderr, "--ERRO: Escrita do vetor 1 no arquivo\n");
         return 5;
     }
 
     ret = fwrite(vetor2, sizeof(float), n, descritorArquivo);
     if (ret < n) {
-        fprintf(stderr, "Erro de escrita do vetor 1 no arquivo\n");
+        fprintf(stderr, "--ERRO: Escrita do vetor 2 no arquivo\n");
         return 5;
     }
 
-    ret = fwrite(&produtoInterno, sizeof(double), 1, descritorArquivo);
-    if (ret < n) {
-        fprintf(stderr, "Erro de escrita do produto interno no arquivo\n");
-        return 6;
-    }
+    ret = fwrite(&prodInt, sizeof(double), 1, descritorArquivo);
 
     #ifdef TEXTO
     fprintf(stdout, "%ld\n", n);
     for(long int i=0; i<n; i++) { //mostra valores do vetor1
         fprintf(stdout, "%f ", vetor1[i]);
     }
+
     fprintf(stdout, "\n");
     for(long int i=0; i<n; i++) { //mostra valores do vetor2
         fprintf(stdout, "%f ", vetor2[i]);
     }
-    fprintf(stdout, "\n");
-    fprintf(stdout, "Produto Interno = %lf\n", produto_interno);
+
+    fprintf(stdout, "\nProduto Interno = %lf\n", prodInt);
     #endif
 
     // Finaliza o uso das variáveis
--- lab2/prod_int_vets_conc.c
+++ lab2/prod_int_vets_conc.c
@@ -4,15 +4,17 @@
 
 // Variáveis globais
 long int n;
-float *vet1, *vet2;
+float *vetor1, *vetor2;
 int nthreads;
-double produto_concorrente_global = 0.0;
+double prodIntGlobal = 0.0;
+float *prodIntRetThread; //Para pegar valor parcial do produto interno retornado pelas threads
 
 // Função executada pelas threads
-void *ProdutoInterno(void *tid) {
+void *CalcProdInt(void *tid) {
     long int id = (long int) tid;
     int ini, fim, bloco;
-    double produto_local = 0.0;
+    float *ret; // produto interno calculado de maneira local
+    double prodIntParcial = 0.0;
 
     bloco = n/nthreads;
     ini = id*bloco;
@@ -20,22 +22,19 @@ void *ProdutoInterno(void *tid) {
     if (id == (nthreads - 1)) fim = n;
 
     for (int i = ini; i < fim; i++) {
-        produto_local += vet1[i] * vet2[i];
+        prodIntParcial += vetor1[i] * vetor2[i];
     }
 
     //retorna o resultado do produto interno 
     ret = malloc(sizeof(float));
-    if (ret!=NULL) *ret = produto_local;
+    if (ret!=NULL) *ret = prodIntParcial;
     else printf("--ERRO: malloc() thread\n");
     pthread_exit((void*) ret);
 }
 
 int main(int argc, char *argv[]) {
-    FILE *arq;
-    size_t ret;
-
-    double produto_interno_arquivo;
-    pthread_t *tid_sistema;
+    FILE *arq; //arquivo de entrada
+    size_t ret; //retorno da funcao de leitura no arquivo de entrada
 
     // Valida e recebe os valores de entrada
     if (argc < 3) {
@@ -53,27 +52,26 @@ int main(int argc, char *argv[]) {
     // Lê o tamanho dos vetores
     ret = fread(&n, sizeof(long int), 1, arq);
     if (!ret) {
-        fprintf(stderr, "Erro de leitura da dimensão dos vetores\n");
+        fprintf(stderr, "--ERRO: Leitura da dimensão dos vetores\n");
         return 3;
     }
 
     // Aloca espaço de memória e carrega os vetores de entrada
     int tamanhoVetores = sizeof(float)*n;
-    vet1 = malloc(tamanhoVetores);
-    vet2 = malloc(tamanhoVetores);
-    if (vet1 == NULL || vet2 == NULL) {
-        printf("--ERRO: malloc()\n");
-        exit(-1);
+    vetor1 = malloc(tamanhoVetores);
+    vetor2 = malloc(tamanhoVetores);
+    if (vetor1 == NULL || vetor2 == NULL) {
+        printf("--ERRO: Alocação de memória para os vetores\n"); exit(-1);
     }
-    ret = fread(vet1, sizeof(float), n, arq);
+    ret = fread(vetor1, sizeof(float), n, arq);
     if (ret < n) {
-        fprintf(stderr, "Erro de leitura dos elementos do vetor 1\n");
+        fprintf(stderr, "--ERRO: Leitura dos elementos do vetor 1\n");
         return 4;
     }
 
-    ret = fread(vet2, sizeof(float), n, arq);
+    ret = fread(vetor2, sizeof(float), n, arq);
     if (ret < n) {
-        fprintf(stderr, "Erro de leitura dos elementos do vetor 2\n");
+        fprintf(stderr, "--ERRO: Leitura dos elementos do vetor 2\n");
         return 4;
     }
 
@@ -82,46 +80,46 @@ int main(int argc, char *argv[]) {
     if (nthreads > n) nthreads = n;
 
     // Aloca espaço para os identificadores das threads no sistema
+    pthread_t *tid_sistema;
     tid_sistema = (pthread_t *) malloc(sizeof(pthread_t) * nthreads);
     if (tid_sistema == NULL) {
-        printf("--ERRO: malloc()\n");
-        exit(-1);
+        printf("--ERRO: malloc()\n"); exit(-1);
     }
 
     // Cria as threads
     for (long int i = 0; i < nthreads; i++) {
-        if (pthread_create(&tid_sistema[i], NULL, ProdutoInterno, (void*) i)) {
-            printf("--ERRO: pthread_create()\n");
-            exit(-1);
+        if (pthread_create(&tid_sistema[i], NULL, CalcProdInt, (void*) i)) {
+            printf("--ERRO: Criação de threads\n"); exit(-1);
         }
     }
 
     // Espera todas as threads terminarem
     for (int i = 0; i < nthreads; i++) {
-        if (pthread_join(tid_sistema[i], NULL)) {
-            printf("--ERRO: pthread_join()\n");
-            exit(-1);
+        if (pthread_join(tid_sistema[i], (void *) &prodIntRetThread)) {
+            printf("--ERRO: Espera da finalização das threads\n"); exit(-1);
         }
+        prodIntGlobal += *prodIntRetThread; free(prodIntRetThread);
     }
 
     // Lê o produto interno registrado no arquivo
-    ret = fread(&produto_interno_arquivo, sizeof(double), 1, arq);
+    double prodIntArq;
+    ret = fread(&prodIntArq, sizeof(double), 1, arq);
     if (ret == 0) {
-        printf("--ERRO: fread()\n");
-        exit(-1);
+        printf("--ERRO: Leitura do produto interno registrado no arquivo\n"); exit(-1);
     }
 
     // Imprime os resultados
-    printf("Produto Interno calculado de maneira concorrente: %.26f\n", produto_concorrente_global);
-    printf("Produto Interno registrado no arquivo...........: %.26lf\n", produto_interno_arquivo);
+    printf("Calculos do Produto Interno:\n");
+    printf("De maneira concorrente..........: %.26f\n", prodIntGlobal);
+    printf("Registrado no arquivo...........: %.26lf\n", prodIntArq);
 
     // Calcula e exibe variância relativa
-    float varRelativa = ((produtoInternoArquivo - produtoInternoGlobal) / produtoInternoArquivo) * 100;
-    printf("Variância Relativa: %.14f\n", varRelativa);
+    float varianciaRelativa = ((prodIntArq - prodIntGlobal) / prodIntArq) * 100;
+    printf("Variância Relativa: %.14f\n", varianciaRelativa);
 
     // Desaloca os espaços de memória
-    free(vet1);
-    free(vet2);
+    free(vetor1);
+    free(vetor2);
     free(tid_sistema);
     fclose(arq);
 
