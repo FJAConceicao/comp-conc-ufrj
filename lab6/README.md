cc320dc77d4966d8500ad0a87a542105f71b146e
Felipe de Jesus <felipe.conceicao@g.globo>
2024-10-08 23:42:24 -0300

    lab 6

---
+++ lab6/README.md
@@ -0,0 +1,65 @@
+# Implementação de Leitor-Escritor em Lista Encadeada com Prioridade para Escrita
+
+## Descrição
+
+Esta implementação resolve o clássico problema de sincronização entre leitores e escritores, priorizando as operações de escrita. Sempre que uma escrita é solicitada, as novas leituras são bloqueadas até que a escrita seja concluída. Para facilitar o monitoramento via logs, o número total de operações foi limitado a 700.
+
+## Compilação e Execução
+
+Para compilar o código, utilize o comando:
+
+```bash
+gcc list_int.c main_lock_rw_priority.c -o main_lock_rw_priority -Wall
+```
+
+Para executar o programa, utilize o seguinte comando:
+
+```bash
+./main_lock_rw_priority <número de threads>
+```
+
+## Alterações realizadas
+
+Reaproveitei boa parte do código base disponibilizado pela professora, com algumas adaptações feitas para atender aos requisitos específicos do laboratório.
+
+Como o objetivo é priorizar os escritores, criei a variável de controle `querEscrever`. Ela assume o valor 1 quando algum escritor está escrevendo ou solicitando escrita, e 0 quando todos os escritores foram atendidos. Dessa forma, uma thread leitora só pode iniciar sua leitura se não houver nenhum escritor ativo ou aguardando para escrever.
+
+Para facilitar os testes, ajustei a distribuição das operações:
+
+* 50% para consultas (leituras);
+* 40% para inserções;
+* 10% para remoções.
+
+A configuração original de apenas 1% para inserção e 1% para remoção dificultava a análise.
+
+Modifiquei as funções de controle de leitores e escritores apresentado nos slides da professora:
+
+* **EntraLeitura()**: As threads leitoras podem iniciar a leitura apenas quando nenhum escritor estiver ativo ou aguardando (ou seja, `querEscrever = 0`). Caso contrário, elas entram em estado de espera (`wait`) até receberem o sinal de que não há mais escritores. Adicionei um `print` para indicar que uma thread leitora está aguardando devido à presença de uma escrita em andamento ou solicitada.
+* **SaiLeitura()**: Aqui, adicionei um `print` para notificar que a thread leitora concluiu sua operação.
+* **EntraEscrita()**: As threads escritoras só começam a escrever quando não há leitores ativos ou outros escritores em ação. Caso contrário, elas entram em espera. Dentro do loop `while` que antecede o `wait`, a variável `querEscrever` é definida como 1 para sinalizar a prioridade da escrita. Adicionei também um `print` para indicar que a thread escritora está em espera.
+* **SaiEscrita()**: Ao terminar a escrita, a variável `querEscrever` é redefinida para 0, e um `print` é exibido para indicar que a thread escritora finalizou sua operação.
+
+## Perguntas do Laboratório
+
+### **Atividade 1**
+
+**1. O TAD lista implementado nesses arquivos poderia ser compartilhado por threads de um programa concorrente? Com qual finalidade e de que forma?**
+
+Sim, o TAD lista pode ser compartilhado entre threads. Isso porque ele oferece uma forma organizada de armazenar dados e realizar operações sobre eles, o que pode ajudar na resolução de problemas. As threads podem usar essa estrutura através de funções que escondem os detalhes da implementação, facilitando o uso da lista de forma segura e eficiente.
+
+**4. O que poderia acontecer se o programa não implementasse exclusão mútua no
+acesso às operações da lista encadeada?**
+
+Sem exclusão mútua, podemos ter problemas de concorrência, onde duas ou mais threads tentam acessar ou modificar a lista ao mesmo tempo. Isso pode causar erros nos dados, como leituras e escritas incorretas, criando inconsistências.
+
+**6. O que acontece com o tempo de execução do programa quando aumentamos o
+número de threads? Por que isso ocorre?**
+
+O tempo de execução pode aumentar à medida que adicionamos mais threads. Isso acontece porque o controle de exclusão mútua exige tempo extra para gerenciar o acesso das threads. O programa gasta mais tempo alternando entre as threads que tentam acessar a lista do que realizando operações na lista em si.
+
+### **Atividade 2**
+
+**4. Em quais cenários o uso do rwlock pode ser mais vantajoso que o uso do lock de
+exclusão mútua?**
+
+O rwlock é melhor em situações onde há muito mais leituras do que escritas. Isso porque ele permite que várias threads leiam os dados ao mesmo tempo, enquanto o lock de exclusão mútua bloqueia todas as threads, mesmo para leitura, o que pode ser menos eficiente.
---
+++ lab6/main_lock_rw_priority.c
@@ -0,0 +1,155 @@
+//Programa concorrente que cria e faz operacoes sobre uma lista de inteiros
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "list_int.h"
+#include <pthread.h>
+#include "timer.h"
+
+#define QTDE_OPS 700 //quantidade de operacoes sobre a lista (insercao, remocao, consulta)
+#define QTDE_INI 100  //quantidade de insercoes iniciais na lista
+#define MAX_VALUE 100 //valor maximo a ser inserido
+
+//lista compartilhada iniciada 
+struct list_node_s* head_p = NULL; 
+
+//qtde de threads no programa, leitores e escritores
+int nthreads, leit, escr;
+int querEscrever = 0; // para controlar quando threads de escrita devem ganhar prioridade
+
+//locks de exclusao mutua e condicionais de leitura e escrita
+pthread_mutex_t mutex;
+pthread_cond_t cond_leit, cond_escr;
+
+void EntraLeitura(long int id) {
+	pthread_mutex_lock(&mutex);
+   printf("Thread leitora %ld tenta ler\n", id);
+	/* Se tem escritor escrevendo (escr > 0) ou
+       querEscrever (alguma thread está querendo escrever),
+       então thread que chega para ler fica em espera */
+	while((escr > 0) || (querEscrever == 1)) {
+      printf("Thread leitora %ld fica esperando, pois tem escritor ativo ou em espera\n", id);
+		pthread_cond_wait(&cond_leit, &mutex);
+	}
+	leit++;
+   printf("Thread leitora %ld começa leitura\n", id);
+	pthread_mutex_unlock(&mutex);
+}
+
+void SaiLeitura(long int id) {
+	pthread_mutex_lock(&mutex);
+	leit--;
+   printf("Thread leitora %ld termina de ler\n", id);
+	if(leit==0) pthread_cond_signal(&cond_escr);
+	pthread_mutex_unlock(&mutex);
+}
+
+void EntraEscrita(long int id) {
+	pthread_mutex_lock(&mutex);
+   printf("Thread escritora %ld tenta escrever\n", id);
+	while((leit>0) || (escr>0)) {
+		querEscrever = 1; // momento em que a thread solicita escrita
+      printf("Thread escritora %ld fica esperando, pois tem threads escritoras ou leitoras ativas\n", id);
+		pthread_cond_wait(&cond_escr, &mutex);
+	}
+	escr++;
+   printf("Thread escritora %ld começa escrita\n", id);
+	pthread_mutex_unlock(&mutex);
+}
+
+void SaiEscrita(long int id) {
+	pthread_mutex_lock(&mutex);
+	escr--;
+   querEscrever = 0; // retorna para 0 indicando que a solicitação de escrita pela thread foi atendida
+   printf("Thread escritora %ld termina escrita\n", id);
+	pthread_cond_signal(&cond_escr);
+	pthread_cond_broadcast(&cond_leit);
+	pthread_mutex_unlock(&mutex);
+}
+
+//tarefa das threads
+void* tarefa(void* arg) {
+   long int id = (long int) arg; 
+   int op;
+   int in, out, read; 
+   in=out=read = 0; 
+
+   //realiza operacoes de consulta (98%), insercao (1%) e remocao (1%)
+   for(long int i=id; i<QTDE_OPS; i+=nthreads) {
+      op = rand() % 100;
+      if(op<50) {
+        EntraLeitura(id);
+        Member(i%MAX_VALUE, head_p);   /* Ignore return value */
+        SaiLeitura(id);
+	    read++;
+      } else if(50<=op && op<90) {
+        EntraEscrita(id);
+        Insert(i%MAX_VALUE, &head_p);  /* Ignore return value */
+        SaiEscrita(id);
+        in++;
+      } else if(99<=op) {
+        EntraEscrita(id);
+        Delete(i%MAX_VALUE, &head_p);  /* Ignore return value */
+        SaiEscrita(id);
+        out++;
+      }
+   }
+   //registra a qtde de operacoes realizadas por tipo
+   printf("Thread %ld: in=%d out=%d read=%d\n", id, in, out, read);
+   pthread_exit(NULL);
+}
+
+/*-----------------------------------------------------------------*/
+int main(int argc, char* argv[]) {
+   pthread_t *tid;
+   double ini, fim, delta;
+   
+   //verifica se o numero de threads foi passado na linha de comando
+   if(argc<2) {
+      printf("Digite: %s <numero de threads>\n", argv[0]); return 1;
+   }
+   nthreads = atoi(argv[1]);
+
+   //insere os primeiros elementos na lista
+   for(int i=0; i<QTDE_INI; i++)
+      Insert(i%MAX_VALUE, &head_p);  /* Ignore return value */
+   
+   //aloca espaco de memoria para o vetor de identificadores de threads no sistema
+   tid = malloc(sizeof(pthread_t)*nthreads);
+   if(tid==NULL) {  
+      printf("--ERRO: malloc()\n"); return 2;
+   }
+
+   //inicializa a variavel mutex
+   pthread_mutex_init(&mutex, NULL);
+   
+   //tomada de tempo inicial
+   GET_TIME(ini);
+   //cria as threads
+   for(long int i=0; i<nthreads; i++) {
+      if(pthread_create(tid+i, NULL, tarefa, (void*) i)) {
+         printf("--ERRO: pthread_create()\n"); return 3;
+      }
+   }
+   
+   //aguarda as threads terminarem
+   for(int i=0; i<nthreads; i++) {
+      if(pthread_join(*(tid+i), NULL)) {
+         printf("--ERRO: pthread_join()\n"); return 4;
+      }
+   }
+
+   //tomada de tempo final
+   GET_TIME(fim);
+   delta = fim-ini;
+   printf("Tempo: %lf\n", delta);
+
+   //libera o mutex
+   pthread_mutex_destroy(&mutex);
+   //libera o espaco de memoria do vetor de threads
+   free(tid);
+   //libera o espaco de memoria da lista
+   Free_list(&head_p);
+
+   return 0;
+}  /* main */
